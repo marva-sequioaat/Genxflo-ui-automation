e21c5fa70f753536bfe6d1e6751cd861a480031d
marva-sequioaat <marva@sequoiaat.com>
2025-03-06 07:05:35 +0000

    5 invalid input tested

--- tests/test_invalid_inputs.py
+++ tests/test_invalid_inputs.py
@@ -26,23 +26,42 @@ def test_main(get_driver):
     page_title=driver.title
     assert page_title=="Genxflo - Nextflow Bioinformatics Pipeline Builder"
 
+   
+def test_not_filled_inputfield(get_driver):
+    driver = get_driver
+    wait = WebDriverWait(driver, 10)
+    login = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="root"]/div/div/div[1]/div[3]/span')))
+    login.click()
+    email_input = wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/div/div/div/div[2]/div[1]/input')))
+    email_input.clear()
+    email_input.send_keys("marva@sequoiaat.com")
+    password_input = wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/div/div/div/div[2]/div[2]/input')))
+    password_input.clear()
+    time.sleep(3)
+    password_input.send_keys(Keys.TAB)
+    signin = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div/div/div/div[2]/button/span')))
+    signin.click()
+    wait.until(lambda d: d.find_element(By.XPATH, '//*[@id="root"]/div/div/div[2]/div[5]/p').text == "Please fill all the details")
+    log_element=get_driver.find_element(By.XPATH,'//*[@id="root"]/div/div/div[2]/div[5]/p')
+    error_message=log_element.text
+    expected_message="Please fill all the details"
+    assert error_message==expected_message
+
 def test_invalid_email(get_driver):
     driver = get_driver
     wait = WebDriverWait(driver, 10)
-       
-    login = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="root"]/div/div/div[1]/div[3]/span')))
-    login.click()
     email_input = wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/div/div/div/div[2]/div[1]/input')))
     email_input.send_keys("marva@sequoi")
     password_input = wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/div/div/div/div[2]/div[2]/input')))
     password_input.send_keys('marva@sequoiaat')
     signin = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div/div/div/div[2]/button/span')))
     signin.click()
-    invalid_email=wait.until(EC.presence_of_element_located((By.XPATH,'//*[@id="root"]/div/div/div[2]/div[5]/p')))
-    error_message=invalid_email.text
+    wait.until(lambda d: d.find_element(By.XPATH, '//*[@id="root"]/div/div/div[2]/div[5]/p').text == "Email is invalid")
+    log_element=get_driver.find_element(By.XPATH,'//*[@id="root"]/div/div/div[2]/div[5]/p')
+    error_message=log_element.text
     expected_message="Email is invalid"
     assert error_message==expected_message
-
+    
 
 def test_wrong_password(get_driver):
     driver = get_driver
@@ -77,5 +96,3 @@ def test_user_not_found(get_driver):
     error_message=log_element.text
     expected_message="User not found"
     assert error_message==expected_message
-    
-    
